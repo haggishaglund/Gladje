f25a37dbf4eec90807ce94f828acacf0b175033e
Linnéa <linnea.lej@gmail.com>
2019-04-20 16:26:16 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,15 @@
 <?php
-
-echo "hej";
-echo "vg?";
-echo"Tjo brate";
-
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel=“stylesheet” href = “<?php bloginfo(‘stylesheet_url’)?>
+    <?php wp_head();?>
+    <title></title>
+  </head>
+  <body>
+    <div id="header_wrapper">
+    <nav class="header_menu"> <?php wp_nav_menu('header_menu');?></nav>
+    </div>
 ?>
