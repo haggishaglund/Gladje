dae01bfded6f174d07a05c2e19dbbcec91bac20f
Linnéa <linnea.lej@gmail.com>
2019-04-20 16:32:00 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,2 +1,21 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php wp_footer();?>
+    <title></title>
+  </head>
+  <body>
+      <div class="footer_parent">
+          <h3>Contact info</h3>
+    <div class="list_wrapper">
+    <ul>
+      <li class="footer_menu" href="">Contact</li>
+      <li class="footer_menu" href="">Contact</li>
+      <li class="footer_menu" href="">Contact</li>
+      <li class="footer_menu" href="">Contact</li>
+    </ul>
+    </div>
+     </div>
+
+  </body>
+</html>
