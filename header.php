cdf7fcac55eda30f50e239cf16edaebba0903f1d
Linnéa <linnea.lej@gmail.com>
2019-07-01 11:43:06 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<?php
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -12,4 +12,3 @@
     <div id="header_wrapper">
     <nav class="header_menu"> <?php wp_nav_menu('header_menu');?></nav>
     </div>
-?>
