6486f33d970b05155f1c12c29a9d72afba98cb11
jossandqvist <josefines.1994@gmail.com>
2019-07-07 22:06:50 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link href="css/style.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i,900,900i&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel=“stylesheet” href="<?php echo bloginfo('stylesheet_url');?>">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i,900,900i&display=swap" rel="stylesheet">
+    <title><?php bloginfo('name'); ?> – <?php bloginfo('description'); ?></title>
     <?php wp_head();?>
     <title></title>
   </head>
