5e474e25ac20fcb902991812d3baecb281b92869
jossandqvist <josefines.1994@gmail.com>
2019-07-08 14:03:57 +0200

    Started hardcoding the footer

--- footer.php
+++ footer.php
@@ -1,13 +1,49 @@
 <?php wp_footer();?>
 
 <footer>
-	<h4>Contact info</h4>
-	<ul>
-		<li class="footer_menu" href="">Contact</li>
-		<li class="footer_menu" href="">Contact</li>
-		<li class="footer_menu" href="">Contact</li>
-		<li class="footer_menu" href="">Contact</li>
-	</ul>
+
+	<div class="footer_section">
+		<h6>Kontakt</h6>
+		<ul>
+			<li><a href="tel:036712160">036 – 71 21 60</a></li>
+			<li><a href="mailto:info@gladjejonkoping.se" target="_top">info@gladjejonkoping.se</a></li>
+		</ul>
+	</div>
+
+	<div class="footer_section">
+		<h6>Hitta hit</h6>
+			<p><strong>Glädje Restaurang & Bar</strong><br/>
+			Västra Storgatan 9<br/>
+			55315 JÖNKÖPING</p>
+	</div>
+
+	<div class="footer_section">
+		<h6>Hyra</h6>
+		<ul>
+			<li><a href="/konferens">Konferens</a></li>
+			<li><a href="/event" target="_top">Event</a></li>
+		</ul>
+	</div>
+
+	<div class="footer_section">
+		<h6>Öppettider</h6>
+		<p><strong>Fredagslunch</strong><br/>
+		11:30 &ndash; 15:00</p>
+
+		<p><strong>À la carte</strong><br/>
+		Mån&ndash;tor &emsp; 17 &ndash; 22<br/>
+		Fre &emsp; 20 &ndash; 22<br/>
+		Lör &emsp; 17 &ndash; 22</p>
+
+		<p><strong>Nattklubb</strong><br/>
+		22 &ndash; 02</p>
+
+		<p><strong>After work</strong><br/>
+		Fre &emsp; 17 &ndash; 20</p>
+		</p>
+
+	</div>
+
 </footer>
 
 </body>
