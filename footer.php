1983c7e86f283cb120674d3b0566030e4ff84db1
jossandqvist <josefines.1994@gmail.com>
2019-07-08 12:14:50 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,21 +1,14 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <?php wp_footer();?>
-    <title></title>
-  </head>
-  <body>
-      <div class="footer_parent">
-          <h3>Contact info</h3>
-    <div class="list_wrapper">
-    <ul>
-      <li class="footer_menu" href="">Contact</li>
-      <li class="footer_menu" href="">Contact</li>
-      <li class="footer_menu" href="">Contact</li>
-      <li class="footer_menu" href="">Contact</li>
-    </ul>
-    </div>
-     </div>
+<?php wp_footer();?>
 
-  </body>
+<footer>
+	<h4>Contact info</h4>
+	<ul>
+		<li class="footer_menu" href="">Contact</li>
+		<li class="footer_menu" href="">Contact</li>
+		<li class="footer_menu" href="">Contact</li>
+		<li class="footer_menu" href="">Contact</li>
+	</ul>
+</footer>
+
+</body>
 </html>
