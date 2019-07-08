62ae2606beaa81ba4849469d9f3643a99f6d6a7f
jossandqvist <josefines.1994@gmail.com>
2019-07-08 13:24:49 +0200

    Updates to the front page

--- front-page.php
+++ front-page.php
@@ -1,13 +1,35 @@
 <?php get_header();?>
 
+<header>
+</header>
 
+<main>
 
+<div class="section">
+	<h5>Öppettider</h5>
+	<p>Lorem ipsum</p>
+</div>
 
+<div class="section">
+	<img src="https://ucee01550c5f4d21bdb508adaf79.previews.dropboxusercontent.com/p/thumb/AAfFXqN1dWaQUYKNGyWicT7YPQw77WN2U5ZXx-jdVMm1raQDHtYOXPPcEtaRbj1RmA_7XB8NxbFPpBSxsNP7UJt8YRhiPdK99chHSX83u0PRjICIVDqOjZ55NemkSQJDBQ7-t09kY0wUAaPKY0TvXFvUEk4PU_Cg6SmI3EQ0Pmpxil-8q08c-Bysb7Y1iE79UzZ48C9f2XaHM9BjXkqCvsNkjz_yrkMSvDs4w4fMVRZkmr1GhNku-_2ON-W9QE6XwuRaLdXrzCtSoD8Sh4v5aqRrj_B0qwK2UqLWqoIjZtMUHohlAuMUBH8j2KZAJ5-OGoxUSFnoaEUt8rUDtoNlN0q52ocN0YYFG9Ugq6ujcwflwQ/p.jpeg?fv_content=true&size_mode=5">
+</div>
 
+<div class="section">
+	<h5>Om glädje</h5>
+	<p>Lorem ipsum</p>
+</div>
 
+<div class="section">
+	<img src="https://uc354433aae910ca530270d6adf4.previews.dropboxusercontent.com/p/thumb/AAdTRtIo0fJyAWXeUXE3usTclCeeN01aE52TMr5DMDgYZZrnmXZxg5HbUf84G4ckINwUSAdNYvzbH87DmOZ3xCjaziCtdM2HIi7PqZ4g9JoxgumTHck3W2eUTpxZssOA2sC2xwSBriY8H9EZIV-gM4_2MvQTINiFNGMFeMn9HkDPCCQhZxwfvwyua2aCcLe9QsvSkEn7DcJLJm7U6RGCDErGiiLAIJEBW_iLyVbUFl9zmzGTezPHjSJ8l5tQU1TUzlVW-1_hIHRTN8VbVt1zpDT0UlyvaUAP7vNvBhlYRsUSr8q7Hmj6PlimkCt3kBUli3MwT-Tt6Ql2ixAd_k-k291why6-7T1jYP4Do7h5Lcn79A/p.jpeg?fv_content=true&size_mode=5">
+</div>
 
+<div class="section">
+	<h5>After work</h5>
+	<p class="sub_heading">Fredagar 17 &ndash; 20</p>
+	<p>Välkommen till After Work på Glädje. Här dukar vi upp en underbar buffé där allt från kött, fisk, sallad, potatis och såser står uppdukat. I baren erbjuder vi självklart reducerade priser på öl, vin och alkoholfritt.</p>
+	<p>Vår After Work är väldigt populär så passa på att boka bord för ert sällskap. Varmt välkomna!</p>
+</div>
 
-
-
+<main>
 
 <?php get_footer(); ?>
