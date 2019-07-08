54053efd5ed5021a00f70aa6ee3d527a28008547
jossandqvist <josefines.1994@gmail.com>
2019-07-08 12:12:35 +0200

    Working on the top navigation

--- header.php
+++ header.php
@@ -1,28 +1,27 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url');?>">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i,900,900i&display=swap" rel="stylesheet">
-    <title><?php bloginfo('name'); ?> – <?php bloginfo('description'); ?></title>
-    <?php wp_head();?>
-    <title></title>
-  </head>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url');?>">
+		<link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i,900,900i&display=swap" rel="stylesheet">
+		<title><?php bloginfo('name'); ?> – <?php bloginfo('description'); ?></title>
+		<?php wp_head();?>
+		<title></title>
+	</head>
 
 <body>
 
 <nav id="top_nav">
-  <?php wp_nav_menu('header_menu');?>
+	<?php wp_nav_menu('header_menu');?>
 </div>
 
 <?php
 // I think we need this
 // We do, but not in the header
-        if( have_posts() ){
-            while( have_posts() ){
-      the_post();
-    }
-  }
+	if( have_posts() ){
+		while( have_posts() ){
+			the_post();
+		} // end WHILE have_posts
+	} // end IF have_posts
 ?>
-
