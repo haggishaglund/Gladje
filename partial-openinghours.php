ae2fb2d6000a723c6dc164a99a2241b0e808595f
jossandqvist <47322866+jossandqvist@users.noreply.github.com>
2019-07-09 19:31:32 +0200

    Create partial-openinghours.php
    Partial, då vi kan (dvs borde) använda denna kod även till footern. Då, när man uppdaterar öppettiderna på startsidan så uppdateras de automatiskt i footern med!

---
+++ partial-openinghours.php
@@ -0,0 +1,24 @@
+	<p>
+		<strong><?php the_field('box1_open1_title'); ?></strong><br/>
+		<?php if(get_field('box1_open1_days1')) { the_field('box1_open1_days1'); ?> &emsp; <?php the_field('box1_open1_days1_open'); ?> &ndash; <?php the_field('box1_open1_days1_close'); ?><br/><?php } ?>
+		<?php if(get_field('box1_open1_days2')) { the_field('box1_open1_days2'); ?> &emsp; <?php the_field('box1_open1_days2_open'); ?> &ndash; <?php the_field('box1_open1_days2_close'); ?><br/><?php } ?>
+		<?php if(get_field('box1_open1_days3')) { the_field('box1_open1_days3'); ?> &emsp; <?php the_field('box1_open1_days3_open'); ?> &ndash; <?php the_field('box1_open1_days3_close'); } ?>
+	</p>
+
+		<p><strong><?php the_field('box1_open2_title'); ?></strong><br/>
+			<?php if(get_field('box1_open2_days1')) { the_field('box1_open2_days1'); ?> &emsp; <?php the_field('box1_open3_days1_open'); ?> &ndash; <?php the_field('box1_open2_days1_close'); ?><br/><?php } ?>
+			<?php if(get_field('box1_open2_days2')) { the_field('box1_open2_days2'); ?> &emsp; <?php the_field('box1_open3_days2_open'); ?> &ndash; <?php the_field('box1_open2_days2_close'); ?><br/><?php } ?>
+			<?php if(get_field('box1_open2_days3')) { the_field('box1_open2_days3'); ?> &emsp; <?php the_field('box1_open3_days3_open'); ?> &ndash; <?php the_field('box1_open2_days3_close'); } ?>
+		</p>
+
+		<p><strong><?php the_field('box1_open3_title'); ?></strong><br/>
+			<?php if(get_field('box1_open3_days1')) { the_field('box1_open3_days1'); ?> &emsp; <?php the_field('box1_open3_days1_open'); ?> &ndash; <?php the_field('box1_open3_days1_close'); ?><br/><?php } ?>
+			<?php if(get_field('box1_open3_days2')) { the_field('box1_open3_days2'); ?> &emsp; <?php the_field('box1_open3_days2_open'); ?> &ndash; <?php the_field('box1_open3_days2_close'); ?><br/><?php } ?>
+			<?php if(get_field('box1_open3_days3')) { the_field('box1_open3_days3'); ?> &emsp; <?php the_field('box1_open3_days3_open'); ?> &ndash; <?php the_field('box1_open3_days3_close'); } ?>
+		</p>
+
+		<p><strong><?php the_field('box1_open4_title'); ?></strong><br/>
+			<?php if(get_field('box1_open4_days1')) {the_field('box1_open4_days1'); ?> &emsp; <?php the_field('box1_open4_days1_open'); ?> &ndash; <?php the_field('box1_open4_days1_close'); ?><br/><?php } ?>
+			<?php if(get_field('box1_open4_days2')) {the_field('box1_open4_days2'); ?> &emsp; <?php the_field('box1_open4_days2_open'); ?> &ndash; <?php the_field('box1_open4_days2_close'); ?><br/><?php } ?>
+			<?php if(get_field('box1_open4_days3')) {the_field('box1_open4_days3'); ?> &emsp; <?php the_field('box1_open4_days3_open'); ?> &ndash; <?php the_field('box1_open4_days3_close'); } ?>
+		</p>
