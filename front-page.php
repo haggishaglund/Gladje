2beeef6cfab5098d8d010cd730108285c89bb941
jossandqvist <josefines.1994@gmail.com>
2019-07-11 13:43:12 +0200

    Fix på startsidan
    Front-page: All text på startsidan är nu dynamisk (går att ändra på startsidan). Självklart måste vi ställa in startsida på Glädje sen och lägga in texten där osv.
    CSS: Ändrat ordningen på vissa properties så de är i bokstavsordning. Stylat länken under afterwork-boxen på startsidan.

--- front-page.php
+++ front-page.php
@@ -11,23 +11,27 @@
 </div>
 
 <div class="section">
-	<img src="https://uc7975af365f723749d561e033e9.previews.dropboxusercontent.com/p/thumb/AAdO02tTaJU8AXsqZ9nXMd5fc1MbXELMZl4kd4oiomkHFiq9vg2pJg45VP5fMr7JQyFJFvNyXzxAkFC_7JP28r_M2sbeI1WHoLgHM7_AQnXOAyYTMyhDFLws4tPklRwes_S97BHPX_tjuPwlFSTckG_28KzfrrGWao5FgmcLwMCTVAAoYHYzfKXVQGewFMIWfbHIhM7E0c8pSaVkk4zbwqWJ2yY8bpDJ6eITUZyquMW1Gw_TITEdxhfSUyxZHdASQ1qBTrjIWOmw0EZEkT3rTruh6lXuOXGpaD9WwezWgRpOnnR1N-mJT0x4IGbWbCyEnOMOmJ2dopVhcptG3eSCWSJIEChWuVBrsjmnxIznrsMXeQ/p.jpeg?fv_content=true&size_mode=5">
+	<img src="https://cdn2.cdnme.se/3450789/9-3/hjalpreda_5d271c1e9606ee11b7434838.jpg">
 </div>
 
 <div class="section">
-	<h5>Om glädje</h5>
-	<p>Lorem ipsum.</p>
+	<h5><?php the_field('box2_title'); ?></h5>
+	<?php the_field('box2_text'); // this field creates automatic paragraphs (<p>) ?>
 </div>
 
 <div class="section">
-	<img src="https://uc61b51dbde6e9e1721158f5cab3.previews.dropboxusercontent.com/p/thumb/AAdh9SU0CWwozCxWLFS--TJoqguov_27w52F_ATclZ2ZS5IseZNCDuPJOyR6Fot9EM9NyVfCXq3wXXNIvKMRG6hU1KKWED479VoNQIT53fsXEIF7Mk-1Y-fD7eAUod6GzR1JdCtWyQ-3fzAL1SsYdMZdVUKXiDKWVbUpJA5bySVn7u1VZmZpon2OOyW2ehQIYCO3aIJ_M6Y49n1RO3aBErd8llG2Yqq0xGT8BmB_KsvuySefK9B2Dj2TOop-nNkJx-XrfcIiUALYeVSBQbSPXcWOucPXP7WwdilHNuE8U6-uDoco49NUtQH16NLC2QCqajVoyoVE-8mDnccws-g39DRIMJfiO9q0sseX4iisuZ2TGA/p.jpeg?fv_content=true&size_mode=5">
+	<img src="https://cdn3.cdnme.se/3450789/9-3/19_5d271c1b9606ee119f801483.jpg">
 </div>
 
 <div class="section">
-	<h5>After work</h5>
-	<p class="sub_heading">Fredagar 17 &ndash; 20</p>
-	<p>Välkommen till After Work på Glädje. Här dukar vi upp en underbar buffé där allt från kött, fisk, sallad, potatis och såser står uppdukat. I baren erbjuder vi självklart reducerade priser på öl, vin och alkoholfritt.</p>
-	<p>Vår After Work är väldigt populär så passa på att boka bord för ert sällskap. Varmt välkomna!</p>
+	<h5><?php the_field('box3_title'); ?></h5>
+
+	<?php if(get_field('box3_days')) { ?>
+		<p class="sub_heading"><?php the_field('box3_days'); ?> &emsp; <?php the_field('box3_open'); ?> &ndash; <?php the_field('box3_close'); ?></p>
+	<?php } // end if(get_field(box3_days) ?>
+	<?php the_field('box3_text'); // this field creates automatic paragraphs (<p>) ?>
+	<a href="<?php the_field('box3_url'); ?>"><div class="section_link"><?php the_field('box3_link'); ?><div class="section_link_arrow">&#8227;</div></div></a>
+	<div class="space"></div>
 </div>
 
 <main>
