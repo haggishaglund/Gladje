04900d02f3b46dfb2a9f31b1c5456903cabaf200
jossandqvist <josefines.1994@gmail.com>
2019-07-11 13:46:53 +0200

    Fixade tider på lunch
    Lol tiderna hade blivit lite fel på lunchen. Fixat nu

--- openinghours.php
+++ openinghours.php
@@ -5,9 +5,9 @@
 </p>
 
 <p><strong><?php the_field('box1_open2_title'); ?></strong><br/>
-	<?php if(get_field('box1_open2_days1')) { the_field('box1_open2_days1'); ?> &emsp; <?php the_field('box1_open3_days1_open'); ?> &ndash; <?php the_field('box1_open2_days1_close'); ?><br/><?php } ?>
-	<?php if(get_field('box1_open2_days2')) { the_field('box1_open2_days2'); ?> &emsp; <?php the_field('box1_open3_days2_open'); ?> &ndash; <?php the_field('box1_open2_days2_close'); ?><br/><?php } ?>
-	<?php if(get_field('box1_open2_days3')) { the_field('box1_open2_days3'); ?> &emsp; <?php the_field('box1_open3_days3_open'); ?> &ndash; <?php the_field('box1_open2_days3_close'); } ?>
+	<?php if(get_field('box1_open2_days1')) { the_field('box1_open2_days1'); ?> &emsp; <?php the_field('box1_open2_days1_open'); ?> &ndash; <?php the_field('box1_open2_days1_close'); ?><br/><?php } ?>
+	<?php if(get_field('box1_open2_days2')) { the_field('box1_open2_days2'); ?> &emsp; <?php the_field('box1_open2_days2_open'); ?> &ndash; <?php the_field('box1_open2_days2_close'); ?><br/><?php } ?>
+	<?php if(get_field('box1_open2_days3')) { the_field('box1_open2_days3'); ?> &emsp; <?php the_field('box1_open2_days3_open'); ?> &ndash; <?php the_field('box1_open2_days3_close'); } ?>
 </p>
 
 <p><strong><?php the_field('box1_open3_title'); ?></strong><br/>
