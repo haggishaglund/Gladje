e21d33e3730f1fd1190f27547a74acbb82daacd0
jossandqvist <josefines.1994@gmail.com>
2019-07-14 17:44:38 +0200

    Fix med After work
    Lång ifrån klar

--- page-aw.php
+++ page-aw.php
@@ -7,22 +7,35 @@
 <main>
 
 <?php while( have_posts() ){ ?>
-<?php the_post(); ?>
+	<?php the_post(); ?>
 
-<div class="section">
-	<h5><?php the_title(); ?></h5>
-	<?php the_content(); ?>
-</div>
-
+	<div class="section">
+		<h5><?php the_title(); ?></h5>
+		<?php the_content(); ?>
+	</div>
 <?php } ?> <!-- end WHILE(HAVE_POSTS) -->
 
 
 
 <div class="section">
-	<h5>Prislista</h5>
+	<h5><?php the_field('price_title'); ?></h5>
+
+	<p>
+
+	<?php if(get_field('menu1_title')) {
+
+		if(get_field('menu1_title')) { ?><div class="item"><?php the_field('menu1_title'); ?></div><?php }
+		if(get_field('menu1_price')) { ?><div class="item_price"><?php the_field('menu1_price'); ?> kr </div><?php }
+		if(get_field('menu1_description')) { ?><div class="item_desc"><?php the_field('menu1_description'); ?></div><?php }
+		if(get_field('menu1_ordpris')) { ?><div class="subtle_text">ord. <?php the_field('menu1_ordpris'); ?> kr </div><?php }
+	
+	} /* end if(get_field(menu1_title) */ ?>
+
+	</p>
+
 </div>
 
-
+<div class="section"><img src="<?php the_field('price_img'); ?>"></div>
 
 </main>
 
