bdd9a41d9a732f9accd8918a75b7837b9a6ff698
jossandqvist <josefines.1994@gmail.com>
2019-07-14 15:25:04 +0200

    Börjar på AW

--- page-aw.php
+++ page-aw.php
@@ -1,19 +1,24 @@
 <?php get_header();?>
 
+<header>
+	<?php the_post_thumbnail('hero_image'); ?>
+</header>
 
+<main>
 
+<?php while( have_posts() ){ ?>
+<?php the_post(); ?>
 
+<div class="section">
 
+	<h5><?php the_title(); ?></h5>
 
+	<?php the_content(); ?>
 
+</div>
 
+<?php } ?> <!-- end WHILE(HAVE_POSTS) -->
 
-
-
-
-
-
-
-
+</main>
 
 <?php get_footer(); ?>
