f10279842fbca2aa2e044f33afb962b3c387df2a
jossandqvist <josefines.1994@gmail.com>
2019-07-19 12:50:21 +0200

    Mall för nya sidor
    Fixade en standard layout för när nya standardsidor skapas bara.

--- page.php
+++ page.php
@@ -1,19 +1,20 @@
 <?php get_header();?>
 
+<header>
+	<?php the_post_thumbnail('hero_image'); ?>
+</header>
 
+<main id="single_main">
 
+<?php while( have_posts() ){ ?>
+	<?php the_post(); ?>
 
+	<div class="section">
+		<h5><?php the_title(); ?></h5>
+		<?php the_content(); ?>
+	</div>
+<?php } ?> <!-- end WHILE(HAVE_POSTS) -->
 
-
-
-
-
-
-
-
-
-
-
-
+</main>
 
 <?php get_footer(); ?>
