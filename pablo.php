d6486b25495f1af5d0d4f015631b3e03032ef70d
Linnéa <linnea.lej@gmail.com>
2019-07-24 00:54:25 +0200

    some stuff with pablo

--- pablo.php
+++ pablo.php
@@ -12,57 +12,63 @@ else {
  wp_head();
  ?>
 
- <body class="pablo_main">
-
  <?php
  echo"<header>";
  the_post_thumbnail('hero_image');
-  echo"</header>";
 
  if( have_posts() ){
    while( have_posts() ){
        the_post();
-
       echo" <div class='pablo_hero_container'>";
-         echo"<div class='pablo_hero_link'>BOKA BORD</div>";
+         echo"<a href='/boka-bord'> <div class='pablo_hero_link'> BOKA BORD </div> </a>";
       echo"</div>";
-
-      /* echo"<div class='pablo_section'>";
-         echo"<h5 id='pablo_title1'>"; the_title(); echo"</h5>";
-         echo"<div class='pablo_content'>"; the_content(); echo"</div>";
-         echo"<div class='pablo_grid_image'>"; the_post_thumbnail('grid_image'); echo"</div>";
-         echo"</div>";*/
    }
- }
+echo"</header>";
 
-
-    $args = array(
+   echo"<body class='pablo_main'>";
+    /*$args = array(
     'post_type' => 'Pablo',
     'post_status' => 'publish',
     'posts_per_page' => 6,
-  );
+  );*/
+
+    while( have_posts() ){
+            the_post();  }
+      ?>
+
+  <div class="pablo_section">
+    	<?php if(get_field('pablo')) {
+        if(get_field('pablo')) { ?><h5 class="pablo_title1"><?php the_field('pablo'); ?></h5><?php }
+        if(get_field('pablo')) { ?><div class="pablo_content"><?php the_field('pablo'); ?></div><?php }
+        if(get_field('pablo_bild')) { ?><div class="pablo_grid_image"><?php the_field('pablo_bild'); ?></div><?php }
+        }
+        if(get_field('pablo')) {
+  			if(get_field('pablo')) { ?><h5 class="pablo_title1"><?php the_field('pablo'); ?></h5><?php }
+  			if(get_field('pablo')) { ?><div class="pablo_content"><?php the_field('pablo'); ?></div><?php }
+        if(get_field('pablo_bild')) { ?><div class="pablo_grid_image"><?php the_field('pablo_bild'); ?></div><?php }
+      }
+      echo"</div>";
+    }/*
 
     $query = new wp_query($args);
     if ($query->have_posts() ){
 
     while ($query->have_posts()){
     $query->the_post();
-    echo"<div class='section'>";
-    echo"<h5 id='pablo_title1'>"; the_title();  echo"</h5>";
-    the_content(); echo"</div>";
+    echo"<div class='pablo_section'>";
+    echo"<h5 class='pablo_title1'>"; the_title();  echo"</h5>";
+    echo"<div class='pablo_content'>"; the_content(); echo"</div>";
     echo"<div class='pablo_grid_image'>"; the_post_thumbnail('grid_image'); echo"</div>";
-
+    echo"</div>"; //section end
     //the_content();
     }
     }else{
         echo"hello";
-    }
+    }*/
     wp_reset_postdata();
-    echo"</div>";
     echo"</body>";
 
     //footer
-
     echo"<div class='pablo_footer'>";
     if(is_page('pablo')) {
      get_footer('pablo');
@@ -70,4 +76,4 @@ else {
     else {
      get_footer();
     }
-    echo"</div>";
+   echo"</div>";
